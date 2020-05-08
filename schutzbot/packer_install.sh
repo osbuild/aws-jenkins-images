7832eb2a291088291a21fea9a8237a889ac8dd25
Major Hayden <major@redhat.com>
2020-05-08 11:15:52 -0500

    First try at a pipeline file

---
+++ schutzbot/packer_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euxo pipefail
+
+if [[ ! -x /usr/bin/unzip ]];
+  dnf -qy install unzip
+fi
+
+curl --retry 5 -Lso /tmp/packer.zip \
+    https://releases.hashicorp.com/packer/1.5.6/packer_1.5.6_linux_amd64.zip
+
+pushd /tmp
+  unzip packer.zip
+  sudo cp packer /usr/local/bin/packer
+  sudo chmod +x /usr/local/bin/packer
+popd
