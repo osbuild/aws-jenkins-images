c6abca2d4ec4ada7611526150023d80c7fd879fa
Major Hayden <major@redhat.com>
2020-05-08 11:31:35 -0500

    Work around weird packer executable

--- schutzbot/packer_install.sh
+++ schutzbot/packer_install.sh
@@ -10,6 +10,6 @@ curl --retry 5 -Lso /tmp/packer.zip \
 
 pushd /tmp
   unzip packer.zip
-  sudo cp packer /usr/local/bin/packer
-  sudo chmod +x /usr/local/bin/packer
+  sudo cp packer /usr/local/bin/packer.io
+  sudo chmod +x /usr/local/bin/packer.io
 popd
